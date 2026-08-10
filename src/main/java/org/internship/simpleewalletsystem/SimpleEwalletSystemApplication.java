988b21bee74d9db076d0ad8e68d4215b9fe93e4b
Sandy <sandy.eissa@vodafone.com>
2026-08-10 17:23:50 +0300

    Initial Spring Boot project

---
+++ src/main/java/org/internship/simpleewalletsystem/SimpleEwalletSystemApplication.java
@@ -0,0 +1,13 @@
+package org.internship.simpleewalletsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEwalletSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleEwalletSystemApplication.class, args);
+    }
+
+}

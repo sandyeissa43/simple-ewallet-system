4c425db0891529987d9df72386a06d5a020240ef
Sandy <sandy.eissa@vodafone.com>
2026-08-17 22:06:52 +0300

    auto-create wallet when creating user

--- src/main/java/com/vois/simpleewalletsystem/service/impl/WalletServiceImpl.java
+++ src/main/java/com/vois/simpleewalletsystem/service/impl/WalletServiceImpl.java
@@ -8,18 +8,35 @@ import com.vois.simpleewalletsystem.exception.WalletNotFoundException;
 import com.vois.simpleewalletsystem.mapper.WalletMapper;
 import com.vois.simpleewalletsystem.repository.WalletRepository;
 import com.vois.simpleewalletsystem.service.WalletService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.vois.simpleewalletsystem.entity.User;
+
+import java.math.BigDecimal;
+
 
 @Service
+@RequiredArgsConstructor
 public class WalletServiceImpl implements WalletService {
 
-    @Autowired
-    private WalletRepository walletRepository;
+    private final WalletRepository walletRepository;
 
-    @Autowired
-    private WalletMapper walletMapper;
+    private final WalletMapper walletMapper;
+    @Override
+    public WalletResponse createWallet(User user) {
+
+        Wallet wallet = Wallet.builder()
+                .user(user)
+                .balance(BigDecimal.ZERO)
+                .build();
+
+        Wallet savedWallet = walletRepository.save(wallet);
+
+        return walletMapper.toResponse(savedWallet);
+    }
+
+
 
     @Override
     public WalletResponse getWalletById(Long walletId) {
